3f8410ffcfeadeae673422832b5f83f657a7b993
Thenewgrid <wstone934@gmail.com>
2024-07-28 08:28:58 -0700

    Part3
    pop up box was done.

---
+++ scripts/mylist.js
@@ -0,0 +1,82 @@
+const listOfWords = [
+    {
+        word: "lion",
+        hint: "Large big cat with a mane."
+    },
+    {
+        word: "star",
+        hint: "Our sun is one of these."
+    },
+    {
+        word: "fiction",
+        hint: "Very fun genre but untrue."
+    },
+    {
+        word: "route",
+        hint: "Path or road you could take."
+    },
+    {
+        word: "earthquake",
+        hint: "When the earth rumbles underneath."
+    },
+    {
+        word: "variety",
+        hint: "The spice of life."
+    },
+    {
+        word: "feather",
+        hint: "Makes flight achievable for a certain animal."
+    },
+    {
+        word: "cold",
+        hint: "The temperature in winter." 
+    },
+    {
+        word: "opponent",
+        hint: "Someone or something you go up against." 
+    },
+    {
+        word: "helmet",
+        hint: "necessary for some sports."
+    },
+    {
+        word: "athlete",
+        hint: "Professional player of sports."
+    },
+    {
+        word: "studio",
+        hint: "Workspace for artists."
+    },
+    {
+        word: "copper",
+        hint: "number 29 on the periodic table."
+    },
+    {
+        word: "drum",
+        hint: "A type of percussion instrument."
+    },
+    {
+        word: "air",
+        hint: "This thing is all around us."
+    },
+    {
+        word: "parachute",
+        hint: "Useful when falling from great heights."
+    },
+    {
+        word: "ambiguity",
+        hint: "Unclear and sometimes confusing."
+    },
+    {
+        word: "breakfast",
+        hint: "The first meal of the day."
+    },
+    {
+        word: "routine",
+        hint: "Something you go through everyday."
+    },
+    {
+        word: "quality",
+        hint: "Often better than quantity."
+    }
+];
